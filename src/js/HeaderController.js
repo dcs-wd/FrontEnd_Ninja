e68d530e0c476944bbcf287d22b10f24dfffaa81
Daniel Castillo <dcsepulveda@hotmail.es>
2018-03-11 21:49:21 +0100

    add form to addarticle

--- src/js/HeaderController.js
+++ src/js/HeaderController.js
@@ -1,12 +1,10 @@
-
-
 export class HeaderController {
-
-    constructor(selector, appController) {
-        this.element = document.querySelector(selector);
-        this.element.querySelector('.fa').addEventListener("click", event => {
-            appController.toggleForm();
-        });
-    }
-
+  constructor(selector, appController) {
+    this.element = document.querySelector(selector);
+    this.element
+      .querySelector(".add-article")
+      .addEventListener("click", event => {
+        appController.toggleForm();
+      });
+  }
 }
